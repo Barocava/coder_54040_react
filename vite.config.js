9e8c3c3972a6035be1e8e4b14ac800a0e8c4d085
barocava <brunodavid1992@gmail.com>
2024-02-13 18:07:18 -0300

    Se actualiza vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/coder_54040_react/",
+  build: {outDir:"docs"},
 })
