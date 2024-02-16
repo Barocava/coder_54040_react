8567e46a02109782ef089216e5f85aeff590aaaa
barocava <brunodavid1992@gmail.com>
2024-02-16 18:13:21 -0300

    Se implementa hook useEffect

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,48 +1,64 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 import "./NavBar.css"
 
 const NavBar = ({ mensaje }) => {
-    const [class_state_acerca, setClases_acerca] = useState("nav-link efecto")
-    const [class_state_como, setClases_como] = useState("nav-link efecto")
-    const [class_state_proyectos, setClases_proyectos] = useState("nav-link efecto active")
-    const [class_state_contacto, setClases_contacto] = useState("nav-link efecto")
+    const [class_state_acerca, setClases_acerca] = useState("nav-link efecto");
+    const [class_state_como, setClases_como] = useState("nav-link efecto");
+    const [class_state_proyectos, setClases_proyectos] = useState("nav-link efecto active");
+    const [class_state_contacto, setClases_contacto] = useState("nav-link efecto");
 
     const handleClick_acerca = (event) => {
         event.preventDefault();
-        if (!event.currentTarget.className.split(" ").includes("active")) {
-            setClases_acerca("nav-link efecto active");
+        setClases_acerca("nav-link efecto active");
+    };
+
+    useEffect(() => {
+        if(class_state_acerca=="nav-link efecto active") {
             setClases_como("nav-link efecto");
             setClases_proyectos("nav-link efecto");
             setClases_contacto("nav-link efecto");
-        }
-    }
+        }      
+    },[class_state_acerca]);
+
     const handleClick_como = (event) => {
         event.preventDefault();
-        if (!event.currentTarget.className.split(" ").includes("active")) {
+        setClases_como("nav-link efecto active");
+    };
+
+    useEffect(() => {
+        if(class_state_como=="nav-link efecto active") {
             setClases_acerca("nav-link efecto");
-            setClases_como("nav-link efecto active");
             setClases_proyectos("nav-link efecto");
-            setClases_contacto("nav-link efecto");
+            setClases_contacto("nav-link efecto");        
         }
-    }
+    },[class_state_como]);
+
     const handleClick_proyectos = (event) => {
         event.preventDefault();
-        if (!event.currentTarget.className.split(" ").includes("active")) {
+        setClases_proyectos("nav-link efecto active");
+    };
+
+    useEffect(() => {
+        if(class_state_proyectos=="nav-link efecto active") {
             setClases_acerca("nav-link efecto");
             setClases_como("nav-link efecto");
-            setClases_proyectos("nav-link efecto active");
-            setClases_contacto("nav-link efecto");
-        }
-    }
+            setClases_contacto("nav-link efecto"); 
+        }       
+    },[class_state_proyectos]);
+
     const handleClick_contacto = (event) => {
         event.preventDefault();
-        if (!event.currentTarget.className.split(" ").includes("active")) {
+        setClases_contacto("nav-link efecto active");
+    };
+
+    useEffect(() => {
+        if(class_state_contacto=="nav-link efecto active") {
             setClases_acerca("nav-link efecto");
             setClases_como("nav-link efecto");
-            setClases_proyectos("nav-link efecto");
-            setClases_contacto("nav-link efecto active");
-        }
-    }
+            setClases_proyectos("nav-link efecto");      
+        }  
+    },[class_state_contacto]);
 
     return (
     <header>
