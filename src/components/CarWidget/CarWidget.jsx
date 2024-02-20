4d4f71f5e785a49538238263076e8264f00cec25
barocava <brunodavid1992@gmail.com>
2024-02-20 11:56:44 -0300

    Se cambia color del badge del carrito

--- src/components/CarWidget/CarWidget.jsx
+++ src/components/CarWidget/CarWidget.jsx
@@ -8,7 +8,7 @@ const CarWidget = ({stock}) => {
 
 <a href="" className="position-relative" onClick={handleClick}>
   <i className="bi bi-cart"></i>
-  <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-info">
+  <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
     {stock}
     <span className="visually-hidden">productos en el carrito</span>
   </span>
