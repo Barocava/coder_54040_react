203f2f99292332ea746f83b99b9fbb3bdf5604ff
barocava <brunodavid1992@gmail.com>
2024-03-30 18:06:35 -0300

    Se agrega el redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import {CartProvider} from '../context/CartContext.jsx'
 import { CasaProvider } from '../context/CasaContext.jsx'
 import { Checkout } from './components/Checkout/Checkout.jsx'
@@ -16,11 +16,12 @@ function App() {
     <CartProvider>
       <NavBar />
       <Routes>
-        <Route path="/coder_54040_react/" element={<ItemListContainer/>}/>
+      <Route path="/coder_54040_react/" element={<Navigate to="/coder_54040_react/category/proyectos" />} />
         <Route path="/coder_54040_react/category/:categoryId" element={<ItemListContainer/>}/>
         <Route path='/coder_54040_react/detail/:productId' element={<ItemListContainer />}/>
         <Route path='/coder_54040_react/checkout' element={<Checkout />}/>
         <Route path='/coder_54040_react/checkout/pago' element={<Formulario />}/>
+        
       </Routes>
       <Footer />
       </CartProvider>
